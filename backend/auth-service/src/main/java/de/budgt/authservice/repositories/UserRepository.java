89a94d6b3f79e4cef9b853d0e8f9cebf89e0facc
Patrick Hofmann <git@hfmnn.com>
2019-08-11 01:53:41 +0200

    Added Auth/DB logic

--- backend/auth-service/src/main/java/de/budgt/authservice/repositories/UserRepository.java
+++ backend/auth-service/src/main/java/de/budgt/authservice/repositories/UserRepository.java
@@ -1,8 +1,8 @@
 package de.budgt.authservice.repositories;
 
-import de.budgt.authservice.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import de.budgt.authservice.domain.User;
 
 import java.util.Optional;
 
