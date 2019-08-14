a0befb2084d7f48dffb528d67e27b6506eedeaad
Patrick Hofmann <git@hfmnn.com>
2019-08-14 23:37:48 +0200

    Fixed auth service Dockerfile

--- backend/account-service/src/main/java/de/budgt/accountservice/dtos/UserRegistrationDto.java
+++ backend/account-service/src/main/java/de/budgt/accountservice/dtos/UserRegistrationDto.java
@@ -1,19 +1,13 @@
 package de.budgt.accountservice.dtos;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 public class UserRegistrationDto implements Serializable {
 
   private static final long serialVersionUID = 5718239471298L;
 
-  @NotNull
-  @NotBlank
   private String username;
 
-  @NotNull
-  @NotBlank
   private String password;
 
   public String getUsername() {
