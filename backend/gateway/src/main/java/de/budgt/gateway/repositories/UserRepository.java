704a61117cf0505581c4f252b2e33d64371037b4
Patrick Hofmann <git@hfmnn.com>
2019-08-15 00:39:14 +0200

    Added login/registration frontend
    Added Login and registratoin component. closes #1 #2 #3

--- backend/gateway/src/main/java/de/budgt/gateway/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package de.budgt.gateway.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import de.budgt.gateway.models.User;
-
-public interface UserRepository extends MongoRepository<User, String> {
-
-  User findByEmail(String email);
-}
