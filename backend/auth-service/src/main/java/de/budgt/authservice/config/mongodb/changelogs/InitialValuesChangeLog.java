20cd05b488d5f3f99a83f3232211ec8a07fd0d7d
Patrick Hofmann <git@hfmnn.com>
2019-12-29 00:33:46 +0100

    Added bank-account-service client credentials to mongobee

--- backend/auth-service/src/main/java/de/budgt/authservice/config/mongodb/changelogs/InitialValuesChangeLog.java
+++ backend/auth-service/src/main/java/de/budgt/authservice/config/mongodb/changelogs/InitialValuesChangeLog.java
@@ -61,4 +61,15 @@ public class InitialValuesChangeLog {
     mongoTemplate.save(categoryServiceClientDetails);
   }
 
+  @ChangeSet(order = "005", id = "insertBankingAccountServiceClientDetails", author = "Patrick Hofmann")
+  public void insertBankingAccountServiceClientDetails(MongoTemplate mongoTemplate) {
+    AuthClientDetails categoryServiceClientDetails = new AuthClientDetails();
+    categoryServiceClientDetails.setClientId("category-service");
+    categoryServiceClientDetails.setClientSecret("$2a$10$fWNTd3H.u7G/aNROVQSifebOkZ2xzU5nUPOCI2Ld42M8E25/ljJqK");
+    categoryServiceClientDetails.setScopes("server");
+    categoryServiceClientDetails.setGrantTypes("refresh_token,client_credentials");
+
+    mongoTemplate.save(categoryServiceClientDetails);
+  }
+
 }
