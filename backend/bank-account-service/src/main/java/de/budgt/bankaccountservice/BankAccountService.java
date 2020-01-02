c898f307811a9e4c1ef06dbda763b3ac190171eb
Patrick Hofmann <git@hfmnn.com>
2020-01-02 03:16:28 +0100

    WIP auth for bank-account-service

--- backend/bank-account-service/src/main/java/de/budgt/bankaccountservice/BankAccountService.java
+++ backend/bank-account-service/src/main/java/de/budgt/bankaccountservice/BankAccountService.java
@@ -2,8 +2,14 @@ package de.budgt.bankaccountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
+@EnableEurekaClient
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BankAccountService {
 
   public static void main(String[] args) {
